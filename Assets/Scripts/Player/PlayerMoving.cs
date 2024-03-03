da98f315e4cca7bd330a379fdca3e07f6fb119db
Vizlo <109802629+DevVizlo@users.noreply.github.com>
2024-03-03 22:20:32 +0300

    Commit fix

--- Assets/Scripts/Player/PlayerMoving.cs
+++ Assets/Scripts/Player/PlayerMoving.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMoving : MonoBehaviour
 {
     private const string _animRun = "Run";
+    private const string _direction = "Horizontal";
 
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
     [SerializeField] private Animator _animator;
 
     private Rigidbody2D _rigidbody2D;
+    private float _powerMovement = 10f;
     private float _horizontalMove = 0f;
 
     private void Start()
@@ -18,7 +21,7 @@ public class PlayerMoving : MonoBehaviour
 
     private void Update()
     {
-        _horizontalMove = Input.GetAxisRaw("Horizontal") * _speed;
+        _horizontalMove = Input.GetAxisRaw(_direction) * _speed;
 
         bool isMove = Input.GetKey(KeyCode.D);
         _animator.SetBool(_animRun, isMove);
@@ -31,7 +34,7 @@ public class PlayerMoving : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 targetVelocity = new Vector2(_horizontalMove * 10f, _rigidbody2D.velocity.y);
+        Vector2 targetVelocity = new Vector2(_horizontalMove * _powerMovement, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = targetVelocity;
     }
 }
